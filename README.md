439625b489b49e98beae84d73b5a40ab07a188ed
glider4d <glider.4d5a@gmail.com>
2025-01-04 11:02:14 +0100

    Commit changes before adding subtree

---
+++ README.md
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install -y build-essential git pkg-config yasm cmake
+sudo apt install -y libx264-dev libx265-dev libvpx-dev libfreetype6-dev libass-dev libfdk-aac-dev libopus-dev libvorbis-dev libtheora-dev libmp3lame-dev libopus-dev libspeex-dev libsdl2-dev
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <libavformat/avformat.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    const char* input_filename = argv[1];
+    AVFormatContext* format_ctx = NULL;
+
+    if (avformat_open_input(&format_ctx, input_filename, NULL, NULL) < 0) {
+        fprintf(stderr, "Could not open input file %s\n", input_filename);
+        return 1;
+    }
+
+    if (avformat_find_stream_info(format_ctx, NULL) < 0) {
+        fprintf(stderr, "Failed to retrieve input stream information\n");
+        return 1;
+    }
+
+    printf("Input file: %s\n", input_filename);
+    printf("Duration: %lld seconds\n", format_ctx->duration / AV_TIME_BASE);
+
+    avformat_close_input(&format_ctx);
+    return 0;
+}
